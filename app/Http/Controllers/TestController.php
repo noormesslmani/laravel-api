9aff33205b13b0440092afa62ed1a0d5268dd055
noormesslmani <noor.messlmani@gmail.com>
2022-09-30 15:58:51 +0300

    created an api that sorts a given string

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -6,5 +6,37 @@ use Illuminate\Http\Request;
 
 class TestController extends Controller
 {
-    //
+    function sortString($string){
+        $stringarr=str_split($string);
+        $chararr=[];
+        $intarr=[];
+        $result=[];
+        foreach ($stringarr as $i) {
+            if ($i<='9' && $i>='0'){
+                array_push($intarr,$i);
+            }
+            else{
+                array_push($chararr,$i);
+            }
+        }
+        sort($intarr);
+        natcasesort($chararr);
+        $chararr=array_values($chararr);
+        for ($i=0;$i<count($chararr)-1;$i++){
+            if(ctype_upper($chararr[$i])==1){
+                for($j=$i+1;$j<count($chararr);$j++){
+                    if(strtolower($chararr[$i])==$chararr[$j]){
+                        $temp=$chararr[$i];
+                        $chararr[$i]=$chararr[$j];
+                        $chararr[$j]=$temp;
+                    }
+                }
+            }
+        }
+        $result=array_merge($chararr, $intarr);
+        $string= implode($result);
+        return response()->json([
+            'result'=>$string
+        ]);
+    }
 }
