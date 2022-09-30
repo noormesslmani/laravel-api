92c4d7bdab8340070f87e4c13112904af383239a
noormesslmani <noor.messlmani@gmail.com>
2022-09-30 18:50:07 +0300

    added comments to all fucntions in the code

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -6,11 +6,13 @@ use Illuminate\Http\Request;
 
 class TestController extends Controller
 {
+    //function that returns a string sorted as aAbB....123...etc
     function sortString($string){
         $stringarr=str_split($string);
         $chararr=[];
         $intarr=[];
         $result=[];
+        //separate letters and integers
         foreach ($stringarr as $i) {
             if ($i<='9' && $i>='0'){
                 array_push($intarr,$i);
@@ -19,9 +21,12 @@ class TestController extends Controller
                 array_push($chararr,$i);
             }
         }
+        //sort integers
         sort($intarr);
+        //case insensitive sort letters 
         natcasesort($chararr);
         $chararr=array_values($chararr);
+        //loop over sorted letters and place uppercase letters after lowercase letters of same type
         for ($i=0;$i<count($chararr)-1;$i++){
             if(ctype_upper($chararr[$i])==1){
                 for($j=$i+1;$j<count($chararr);$j++){
@@ -33,41 +38,47 @@ class TestController extends Controller
                 }
             }
         }
+        //merge letters and numbers arrays and then turn into a string
         $result=array_merge($chararr, $intarr);
         $string= implode($result);
         return response()->json([
             'result'=>$string
         ]);
     }
-
+    //a function that recives a number and returns each place value in the number
     function placeValue($num){
+        //check the sign of the number
         $negative=false;
         if ($num<0){
             $negative=true;
             $num=$num*(-1);
         }
+        //split the number
         $numarr=str_split($num);
+        //multiply each element according to its position in the number 
         for ($i=0;$i<count($numarr);$i++){
             $numarr[$i]=$numarr[$i]*(10**(count($numarr)-1-$i));
             if($negative){
                 $numarr[$i]=$numarr[$i]*(-1);
             }
         }
-
         return response()->json([
             'result'=>$numarr
         ]);
     }
-
+    //function that replaces the numbers in a string with their binary form
     function toBinary($str){
         $arr=str_split($str);
         $newstr='';
         $i=0;
+        //loop over the array of characters in string
         while($i<count($arr)){
             $number='';
+            //if its non numerical character append it to newstr
             if(!($arr[$i]<='9' && $arr[$i]>='0')){
                 $newstr.=$arr[$i];
             }
+            //otherwise loop over numerical characters, convert into binary, then append to newstr
             else{
                 for($j=$i;$j<count($arr);$j++){
                     if(!($str[$j]<='9' && $str[$j]>='0')){
@@ -85,9 +96,13 @@ class TestController extends Controller
             'result'=>$newstr
         ]);
     }
+    //function that takes care of Prefix Notation Evaluation
     function calculate($exp){
+        //split the equation by space
         $equationarr= (explode(" ",$exp));
         $calculate=[];
+        //pop the last element of equationarray and push it into calculate array untill we reach an operand
+        //when an oporand is reached pop the last 2 elements from calculate array, do the operation, then push again into the same array
         while(count($equationarr)>0){
             $x=array_pop($equationarr);
             if($x=='+'){
