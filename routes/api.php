5146a85e89a9e4f52729dd355f68b81913c6546c
noormesslmani <noor.messlmani@gmail.com>
2022-09-30 16:16:34 +0300

    fixed the name of the controller class

--- routes/api.php
+++ routes/api.php
@@ -2,12 +2,13 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\TestController;
 Route::group(['prefix'=>'tes'],function(){
-    Route::get('/sort/{string}',[controller1::class,'sortString']);
-    Route::get('/place/{num}',[controller1::class,'placeValue']);
-    Route::get('/binary/{sentence}',[controller1::class,'toBinary']);
-}
+    Route::get('/sort/{string}',[TestController::class,'sortString']);
+    Route::get('/place/{num}',[TestController::class,'placeValue']);
+    Route::get('/binary/{sentence}',[TestController::class,'toBinary']);
+    Route::get('/operation/{expression}',[TestController::class,'calculate']);
+});
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
